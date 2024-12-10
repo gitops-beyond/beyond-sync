1054dcf02efe8c928c8483c6f6e87e7eaf563111
DanVerh <danverh29@gmail.com>
2024-12-10 23:06:38 +0200

    add env vars

--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
     "github.com/gin-gonic/gin"
     "github.com/gitops-beyond/beyond-sync/handlers"
+    "github.com/joho/godotenv"
+	"os"
+    "log"
 )
 
 func main() {
+    // Load environment variables from .env file
+	godotenv.Load()
+    if os.Getenv("PORT") == "" {
+        log.Fatal("PORT env variable missing")
+    }
+
     r := gin.Default()
 
     r.POST("/api/sync", handlers.TriggerSync)
     r.GET("/api/sync", handlers.GetAllSyncs)
     r.GET("/api/sync/:date", handlers.GetSyncByDate)
 
-    r.Run(":8080")
+    r.Run(":" + os.Getenv("PORT"))
 }
