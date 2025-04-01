996f380ad45e9d64e250892d2b689f3580a0de0d
DanVerh <danverh29@gmail.com>
2025-04-02 00:15:29 +0300

    feat: script for release binaries packaging

---
+++ package.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+
+# Compile go code into binary for Linux ARM
+GOOS=linux GOARCH=arm64 go build -C cmd/api/ -o ../../bin/beyond-sync-api
+GOOS=linux GOARCH=arm64 go build -C cmd/sync/ -o ../../bin/beyond-sync-worker
+
+# Package binaries into tar.gz
+tar --no-xattrs -czvf bin/beyond-sync.tar.gz -C bin beyond-sync-api beyond-sync-worker
