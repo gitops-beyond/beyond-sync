9cead7f91bbf719599ece62bf54027bd7a279446
DanVerh <danverh29@gmail.com>
2025-04-02 01:26:56 +0300

    feat: rename cmd to worker

--- package.sh
+++ package.sh
@@ -2,7 +2,7 @@
 
 # Compile go code into binary for Linux ARM
 GOOS=linux GOARCH=arm64 go build -C cmd/api/ -o ../../bin/beyond-sync-api
-GOOS=linux GOARCH=arm64 go build -C cmd/sync/ -o ../../bin/beyond-sync-worker
+GOOS=linux GOARCH=arm64 go build -C cmd/worker/ -o ../../bin/beyond-sync-worker
 
 # Package binaries into tar.gz
 tar --no-xattrs -czvf bin/beyond-sync.tar.gz -C bin beyond-sync-api beyond-sync-worker
